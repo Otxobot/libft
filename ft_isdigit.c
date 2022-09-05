8d681c55e84ac68bd30b693abd91cd1508e6e988
Otxobot <otxoboy64@gmail.com>
2022-09-05 17:46:07 +0200

    mody

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 12:04:57 by abasante          #+#    #+#             */
-/*   Updated: 2022/08/31 13:39:25 by abasante         ###   ########.fr       */
+/*   Updated: 2022/09/05 17:42:04 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,20 @@
 
 int	ft_isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
+	if (c >= 0 && c <= 9)
 		return (1);
 	return (0);
 }
+
+/*
+int main(void)
+{
+	int i;
+
+	i = 10;
+
+	printf("%i",ft_isdigit(i));
+
+	return(0);
+}
+*/
